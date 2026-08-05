db9e738361a47af5e3968d5f651a3fdecfbdd4e4
Maitreyee <maitreyeechatterjee.info@gmail.com>
2026-08-05 21:09:27 +0530

    Day 1 code

--- task.py
+++ task.py
@@ -1,13 +1,12 @@
-#len("Hello")
+#decription for band name generator project
+# 1. create a greeting for your program
+# 2. ask the user for the city that they grew up in and store it in a variable
+# 3. ask the user for the name of a pet and store it in a variable
+# 4. combine the name of their city and pet and show them their band name
 
-name = "jack"
-print(name)
 
-name = "mona"
-print(name)
 
-print(len(input("what is my name?")))
+#NOW TO GET INPUTS FROM USERS USE THE INPUT FUNCTION ()
+print("Welcome to the Band Name Genrator") 
+city = input("Which city did you grow up in?")
 
-username = input("what is your name?")
-length = len(username)
-print(length)
