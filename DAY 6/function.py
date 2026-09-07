f6dd586a5ebfc02daa13293000469c326c7dc113
Maitreyee Chatterjee <maitreyeechatterjee.info@gmail.com>
2026-09-07 22:03:13 +0530

    Add files via upload

---
+++ DAY 6/function.py
@@ -0,0 +1,17 @@
+# #len and print functions
+
+len("hello")
+print(len("hello"))
+# #or
+input = "hello"
+print(len(input))
+
+
+#how to create our own function?
+
+def my_function():
+    print("hello there")
+    print("how are you?")
+#no output as function is not executed
+#to call the function
+my_function()
